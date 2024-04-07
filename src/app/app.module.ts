903138e804a979043fe69db0001b544503e58eac
snehaljha <snehaljha08@gmail.com>
2024-04-07 18:54:29 +0530

    includes material ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { SearchBarComponent } from './nav-menu/search-bar/search-bar.component';
 import { ListsComponent } from './nav-menu/lists/lists.component';
 import { CreateComponent } from './nav-menu/create/create.component';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -18,9 +23,15 @@ import { CreateComponent } from './nav-menu/create/create.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
