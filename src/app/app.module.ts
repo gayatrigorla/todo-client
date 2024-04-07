e6274f8076dded51746e95a83649cb4562a360ea
gayatrigorla <gayathrineelimachinmay@gmail.com>
2024-04-07 16:20:05 +0530

    Nav-Bar Addition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { SearchBarComponent } from './nav-menu/search-bar/search-bar.component';
+import { ListsComponent } from './nav-menu/lists/lists.component';
+import { CreateComponent } from './nav-menu/create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    SearchBarComponent,
+    ListsComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
