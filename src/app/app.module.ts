9bf2a8e230615840a0d81a2f840a2476f66d9faa
snehaljha <snehaljha08@gmail.com>
2024-04-09 20:20:22 +0530

    create list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,13 @@ import { ListsComponent } from './nav-menu/lists/lists.component';
 import { CreateComponent } from './nav-menu/create/create.component';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { ListItemComponent } from './nav-menu/lists/list-item/list-item.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {MatMenuModule} from '@angular/material/menu';
 
@@ -34,8 +35,9 @@ import {MatMenuModule} from '@angular/material/menu';
     MatIconModule,
     MatDividerModule,
     MatButtonModule,
-    MatFormFieldModule,
     MatInputModule,
+    MatFormFieldModule,
+    MatSidenavModule,
     MatListModule,
     MatMenuModule
   ],
--- src/app/nav-menu/create/create.component.ts
+++ src/app/nav-menu/create/create.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrl: './create.component.css'
+  styleUrl: './create.component.scss'
 })
 export class CreateComponent {
+  listName = '';
 
+  createList() {
+    console.error(this.listName);
+    this.listName = '';
+  }
 }
